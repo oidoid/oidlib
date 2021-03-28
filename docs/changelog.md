9be6c4d57cf0b4dfa7db1a848f1b909087be3b74
Stephen Niedzielski <stephen@niedzielski.com>
2021-03-27 18:55:11 -0600

    [build][API] Rename export from "math" to "matoid"

--- docs/changelog.md
+++ docs/changelog.md
@@ -4,4 +4,5 @@ Versions and bullets are arranged chronologically from latest to oldest.
 
 ## v0.0.1 (unreleased)
 
+- \[build\]\[API\] Rename export from "math" to "matoid"
 - Initial release
---
+++ src/matoid/matoid.ts
@@ -0,0 +1,7 @@
+export {assert} from './assert.js'
+export {Int} from './int.js'
+export type {Millis} from './millis.js'
+export {NumberUtil} from './number-util.js'
+export {Rect, RInt} from './rect.js'
+export {WH, WHInt} from './wh.js'
+export {XY, XYInt} from './xy.js'
