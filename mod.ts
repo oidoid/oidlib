c3257f02b559101f6a9d118f108b46ce11aa1475
Stephen Niedzielski <stephen@niedzielski.com>
2022-11-12 00:12:31 -0700

    Fix bad export

--- mod.ts
+++ mod.ts
@@ -1,13 +1,21 @@
 // ─── oidlib >°──
 
+// Import order is due to https://github.com/denoland/deno/issues/11286.
+// export * from './src/test/TestUtil.ts';
+
+export * from './src/utils/ArrayUtil.ts';
+export * from './src/utils/assert.ts';
+export * from './src/utils/Color.ts';
+export * from './src/utils/NumUtil.ts';
+export * from './src/utils/Obj.ts';
 export * from './src/utils/Str.ts';
+
 export * from './src/types/Num.ts';
 export * from './src/2d/XY.ts';
 export * from './src/2d/Box.ts';
+export * from './src/utils/Str.ts';
 
 export * from './src/math/Random.ts';
-
-export * from './src/test/TestUtil.ts';
 
 export * from './src/time/Millis.ts';
 
@@ -15,10 +23,4 @@ export * from './src/types/Immutable.ts';
 export * from './src/types/Inverse.ts';
 export * from './src/types/JSON.ts';
 export * from './src/types/NonNull.ts';
-
-export * from './src/utils/ArrayUtil.ts';
-export * from './src/utils/assert.ts';
-export * from './src/utils/Color.ts';
-export * from './src/utils/Obj.ts';
-export * from './src/utils/NumUtil.ts';
-export * from './src/utils/Str.ts';
+export * from './src/types/Opt.ts';
