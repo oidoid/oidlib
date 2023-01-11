e93e15f89901753145ed11f89d3eb24d064d05a4
Stephen Niedzielski <stephen@niedzielski.com>
2023-01-11 00:11:35 -0700

    shish-kebab-case filenames

--- mod.ts
+++ mod.ts
@@ -3,17 +3,17 @@
 // Import order is due to https://github.com/denoland/deno/issues/11286.
 // export * from './src/test/TestUtil.ts';
 
-export * from './src/utils/Str.ts';
-export * from './src/types/Num.ts';
-export * from './src/2d/XY.ts';
-export * from './src/2d/Box.ts';
-export * from './src/math/Random.ts';
-export * from './src/types/Immutable.ts';
-export * from './src/types/Inverse.ts';
-export * from './src/types/JSON.ts';
-export * from './src/types/NonNull.ts';
-export * from './src/utils/ArrayUtil.ts';
+export * from './src/utils/str.ts';
+export * from './src/types/num.ts';
+export * from './src/2d/xy.ts';
+export * from './src/2d/box.ts';
+export * from './src/math/random.ts';
+export * from './src/types/immutable.ts';
+export * from './src/types/inverse.ts';
+export * from './src/types/json.ts';
+export * from './src/types/non-null.ts';
+export * from './src/utils/array-util.ts';
 export * from './src/utils/assert.ts';
-export * from './src/utils/Color.ts';
-export * from './src/utils/NumUtil.ts';
-export * from './src/utils/Obj.ts';
+export * from './src/utils/color.ts';
+export * from './src/utils/num-util.ts';
+export * from './src/utils/obj.ts';
