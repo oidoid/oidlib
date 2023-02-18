a956de95d36bdec193efef5228da2f4da3a364bc
Stephen Niedzielski <stephen@niedzielski.com>
2023-02-17 20:38:31 -0700

    Add binFind

--- src/utils/array-util.test.ts
+++ src/utils/array-util.test.ts
@@ -44,3 +44,26 @@ function permute<T>(array: T[], n: number = array.length): T[][] {
 
   return permutations
 }
+
+for (
+  const [name, nums, num, expected] of [
+    ['empty', [], 1, undefined],
+    ['one found', [1], 1, 0],
+    ['one not found', [1], 2, undefined],
+    ['two found', [1, 2], 2, 1],
+    ['two not found', [1, 3], 2, undefined],
+    ['three found', [1, 2, 3], 2, 1],
+    ['three not found', [1, 2, 3], 0, undefined],
+    ['four found', [1, 2, 3, 4], 1, 0],
+    ['four not found', [1, 2, 3, 5], 4, undefined],
+    ['five found', [1, 2, 3, 4, 5], 3, 2],
+    ['five found 2', [1, 2, 3, 4, 5], 5, 4],
+    ['five not found', [1, 2, 3, 4, 5], 6, undefined],
+  ] as const
+) {
+  Deno.test(`binFind: ${name}.`, () =>
+    assertEquals(
+      ArrayUtil.binFind(nums, num, (lhs, rhs) => lhs - rhs),
+      expected,
+    ))
+}
--- src/utils/array-util.ts
+++ src/utils/array-util.ts
@@ -1,4 +1,29 @@
+import { Comparator } from '../types/comparator.ts'
+
 export namespace ArrayUtil {
+  /** For sorted arrays. */
+  export function binFind<T>(
+    vals: readonly T[],
+    val: T,
+    compare: Comparator<T>,
+  ): number | undefined {
+    let min = 0 // inclusive
+    let max = vals.length // exclusive
+    while (min < max) {
+      const mid = min + Math.trunc((max - min) / 2)
+      const comparison = compare(vals[mid]!, val)
+      if (comparison == 0) return mid
+
+      // If item is less than middle, item can only be in the left. Search the
+      // left. mid has been checked and so is correct for exclusive max.
+      if (comparison < 0) min = mid + 1
+      // If item is greater than middle, item can only be in the right. Search
+      // the right. min is inclusive so add one to skip middle which has been
+      // checked.
+      else max = mid
+    }
+  }
+
   /**
    * Shuffle items in place.
    * https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
