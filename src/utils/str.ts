a5adc3f314ddbd90fbd3525c41480d89f9653ce9
Stephen Niedzielski <stephen@niedzielski.com>
2023-03-24 01:04:03 -0600

    Use const type params

--- src/utils/str.ts
+++ src/utils/str.ts
@@ -1,6 +1,6 @@
 export namespace Str {
   /** Converts the first character of str to uppercase. */
-  export function capitalize<T extends string>(str: T): Capitalize<T> {
+  export function capitalize<const T extends string>(str: T): Capitalize<T> {
     if (str[0] == null) return str as Capitalize<T>
     return `${str[0].toLocaleUpperCase()}${str.slice(1)}` as Capitalize<T>
   }
@@ -11,18 +11,20 @@ export namespace Str {
   }
 
   /** Converts all characters of str to lowercase. */
-  export function lowercase<T extends string>(str: T): Lowercase<T> {
+  export function lowercase<const T extends string>(str: T): Lowercase<T> {
     return str.toLocaleLowerCase() as Lowercase<T>
   }
 
   /** Converts the first character of str to lowercase. */
-  export function uncapitalize<T extends string>(str: T): Uncapitalize<T> {
+  export function uncapitalize<const T extends string>(
+    str: T,
+  ): Uncapitalize<T> {
     if (str[0] == null) return str as Uncapitalize<T>
     return `${str[0].toLocaleLowerCase()}${str.slice(1)}` as Uncapitalize<T>
   }
 
   /** Converts all characters of str to uppercase. */
-  export function uppercase<T extends string>(str: T): Uppercase<T> {
+  export function uppercase<const T extends string>(str: T): Uppercase<T> {
     return str.toLocaleUpperCase() as Uppercase<T>
   }
 
